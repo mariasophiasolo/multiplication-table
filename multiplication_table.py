61ecec1f59103f564d99c5d86269f03094fb6f4a
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-27 22:13:26 +0800

    trial

---
+++ multiplication_table.py
@@ -0,0 +1,7 @@
+# create a multiplication table from 1 to 10
+
+# nested loop to generate multiplication table
+table = [[i * j for j in range(1, 11)] for i in range(1, 11)]
+
+# add a new line after each row to improve readability
+print (table)
